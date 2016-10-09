f153f8041025255aa6d0b097dc8d8f96f33eb755
samover <samuel.overloop@gmail.com>
2016-10-09 18:47:12 +0200

    Creates SurveyTemplate class

--- src/surveys/ios_survey.js
+++ src/surveys/ios_survey.js
@@ -1,26 +1,8 @@
-import inquirer  from 'inquirer';
-import { cloneRepository } from './../utils/git';
+import SurveyTemplate from './surveyTemplate';
+import webBoilerplateQuestions from '../questions/boilerplate_questions';
 
-import { projectNameQuestions } from './../questions/general_questions';
-import { bitbucketCredentialQuestions }  from '../questions/bitbucket_questions';
-
-export default class IOSSurvey {
-  start() {
-
-    const questions = [].concat(
-      // ask the project name
-      projectNameQuestions,
-      // ask bitbucket credentials
-      bitbucketCredentialQuestions
-    );
-
-    inquirer.prompt(questions)
-      .then((answers) => {
-        console.log(answers);
-
-        const { boilerPlate, name } = answers;
-        // clone repository
-        cloneRepository(boilerplate, name)
-      });
-  }
+export default class IOSSurvey extends SurveyTemplate {
+    constructor() {
+        super(webBoilerplateQuestions);
+    }
 }
--- src/surveys/web_survey.js
+++ src/surveys/web_survey.js
@@ -1,28 +1,8 @@
-import inquirer  from 'inquirer';
-import { cloneRepository } from './../utils/git';
+import SurveyTemplate from './surveyTemplate';
+import webBoilerplateQuestions from '../questions/boilerplate_questions';
 
-import { webBoilerplateQuestions } from '../questions/boilerplate_questions';
-import { projectNameQuestions } from './../questions/general_questions';
-import { bitbucketCredentialQuestions }  from '../questions/bitbucket_questions';
-
-export default class WebSurvey {
-  start() {
-
-    const questions = [].concat(
-      // ask for which web boilerplate
-      webBoilerplateQuestions,
-
-      // ask the project name
-      projectNameQuestions,
-
-      // ask bitbucket credentials
-      bitbucketCredentialQuestions
-    );
-
-    inquirer.prompt(questions)
-      .then(cloneRepository)
-      .catch(() => {
-            console.log(chalk.green('Error cloning :('));
-      })
-  }
+export default class WebSurvey extends SurveyTemplate {
+    constructor() {
+        super(webBoilerplateQuestions);
+    }
 }
