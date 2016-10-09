e764d04b69841516a93b38d42cf04c2f1aacb0e8
samover <samuel.overloop@gmail.com>
2016-10-09 18:49:40 +0200

    removes use strict, double " and adds export as default

--- src/utils/format-clone-command.js
+++ src/utils/format-clone-command.js
@@ -1,13 +1,15 @@
-'use strict';
-
 import url from 'url';
 import querystring from 'querystring';
 
-export function formatCloneCommand(cloneData) {
+function formatCloneCommand(cloneData) {
     const { boilerplate, name, bitbucketUserName, bitbucketPassword } = cloneData;
     const urlObject = url.parse(boilerplate.repository);
-    const escapedUsername = querystring.escape(bitbucketUserName).replace(/!/g,"%21");
-    const escapedPassword = querystring.escape(bitbucketPassword).replace(/!/g,"%21");
+
+    // urlencode and escape '!' for people with super secure passwords :-)
+    const escapedUsername = querystring.escape(bitbucketUserName).replace(/!/g, '%21');
+    const escapedPassword = querystring.escape(bitbucketPassword).replace(/!/g, '%21');
 
     return `git clone ${urlObject.protocol}//${escapedUsername}:${escapedPassword}@${urlObject.host}${urlObject.pathname} ${name}`;
 }
+
+export { formatCloneCommand as default };
