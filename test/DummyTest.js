59758abf28ebab95a356cbd5831a2c1e448dc455
samover <samuel.overloop@gmail.com>
2016-10-09 18:44:01 +0200

    Updates tests and test script

--- test/DummyTest.js
+++ test/DummyTest.js
@@ -1,21 +1,16 @@
-'use strict';
-
-import assert from 'assert';
 import chai from 'chai';
 import chaiThings from 'chai-things';
 
-chai.should();
 chai.use(chaiThings);
+const expect = chai.expect;
 
-describe('Project Types', function describe() {
-  const projectTypes = require('./../src/constants').projectTypes;
+const projectTypes = require('./../src/constants').projectTypes;
 
-
-  it('should have a Survey for every ProjectType', function test() {
-    projectTypes.should.all.have.property('survey');
+describe('Project Types', () => {
+  it('should have a Survey for every ProjectType', () => {
+    expect(projectTypes).to.all.have.property('survey');
   });
-
-  it('should have a survey function for every ProjectType', function test() {
-    assert.typeOf(projectTypes[0].survey, 'function');
+  it('should have a survey function for every ProjectType', () => {
+    expect(projectTypes[0].survey).to.be.a('string');
   });
 });
