8915626a91c302860814217e7e5e2abef865b3bc
samover <samuel.overloop@gmail.com>
2016-10-09 18:51:11 +0200

    Updates Ollie class with constructor

--- src/Ollie.js
+++ src/Ollie.js
@@ -1,44 +1,48 @@
 import _ from 'lodash';
 import inquirer from 'inquirer';
 import clear from 'clear';
-// import sleep from 'sleep';
 import figlet from 'figlet';
 import chalk from 'chalk';
 
-import { projectTypes } from './constants';
-import surveys from './surveys';
-
-
 export default class Ollie {
-  static welcome() {
-    console.log(figlet.textSync('Oli', 'Standard'));
-    console.log(chalk.blue('Hi there!'));
-    console.log(chalk.blue('Let\'s get you started with a project...'));
+    constructor(options) {
+        this.projectTypes = options.projectTypes;
+        this.surveys = options.surveys;
+        this.welcome = () => {
+            clear();
+            console.log(figlet.textSync('Oli', 'Standard'));
+            console.log(chalk.blue('Hi there!'));
+            console.log(chalk.blue('Let\'s get you started with a project...'));
+        };
+    }
 
-    // wait 2 seconds
-    // sleep.sleep(1);
-  }
+    openingQuestions() {
+        return [
+            {
+                name: 'projectType',
+                message: 'What kind of project would you like to generate?',
+                type: 'list',
+                choices: _.map(this.projectTypes, 'name'),
+            },
+        ];
+    }
 
-  basic() {
-    clear();
-    Ollie.welcome();
-
-    const questions = [
-      {
-        name: 'projectType',
-        message: 'What kind of project would you like to generate?',
-        type: 'list',
-        choices: _.map(projectTypes, 'name'),
-      },
-    ];
-
-
-    inquirer.prompt(questions)
-      .then((answers) => {
-        const project = _.find(projectTypes, { name: answers.projectType });
-        const Survey = surveys[project.survey];
+    startSurvey(answers) {
+        const project = _.find(this.projectTypes, { name: answers.projectType });
+        const Survey = this.surveys[project.survey];
         const survey = new Survey();
         survey.start();
-      });
-  }
+    }
+
+    basic() {
+        this.welcome();
+        inquirer.prompt(this.openingQuestions())
+            .then((answers) => {
+                this.startSurvey(answers);
+            })
+            .catch((err) => {
+                console.error(err);
+                console.info(err.stack);
+            });
+    }
 }
