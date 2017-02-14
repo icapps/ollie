5932524c1c34b058858e07b0e954626f299c4d39
hoetmaaiers <houdmeyers@gmail.com>
2017-02-14 13:32:41 +0100

    refactor SeekAndReplace into generic utils

--- src/surveys/web_survey.js
+++ src/surveys/web_survey.js
@@ -1,17 +1,15 @@
-import inquirer from 'inquirer';
-import SeekAndReplace from 'seek-and-replace';
-import yaml from 'js-yaml';
-import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
+import inquirer from 'inquirer';
 
 import SurveyTemplate from './surveyTemplate';
-import webBoilerplateQuestions from '../questions/boilerplate_questions';
+import { webBoilerplateQuestions } from '../questions/boilerplate_questions';
 import projectQuestions from './../questions/general_questions';
 import createRepositoryQuestions from './../questions/git_questions';
 
 import GitHandler from '../utils/gitHandler';
 import LocalRepository from './../utils/localRepository';
+import seekAndReplace from './../utils/seek-and-replace';
+import { getOllieBoilerplateConfig } from './../utils/ollie-utils';
 
 const questions = [].concat(
   webBoilerplateQuestions,
@@ -27,38 +25,14 @@ export default class WebSurvey extends SurveyTemplate {
   process(answers) {
     const localRepository = new LocalRepository(answers.localRepositoryPath, answers.projectName);
     const gitHandler = new GitHandler(answers, localRepository);
+    const projectPath = path.join(answers.localRepositoryPath, answers.projectName);
 
     return Promise.resolve()
       .then(() => localRepository.createLocalDirectory())
       .then(() => gitHandler.cloneTemplate())
-      .then(() => {
-        const ollieConfig = yaml.safeLoad(fs.readFileSync(path.join(answers.localRepositoryPath, answers.projectName, 'ollie.yml'), 'utf8'));
-        return inquirer.prompt(ollieConfig.replacementQuestions)
-      })
-      .then((replacementAnswers) => {
-        const namespace = 'OLLIE';
-        const replacePath = path.join(answers.localRepositoryPath, answers.projectName);
-
-        const keyDefinitionsFromConfig = _.map(replacementAnswers, function (value, key) {
-          return {
-            key,
-            replacement: value,
-          }
-        });
-
-        const keyDefinitions = _.merge([], keyDefinitionsFromConfig, [
-          {
-            key: 'NAME',
-            replacement: answers.projectName,
-          }
-        ]);
-
-        const replacer = new SeekAndReplace(namespace, replacePath, keyDefinitions, {
-          ignorePaths: ['node_modules', '.idea', '.git']
-        });
-
-        return replacer.replace();
-      })
+      .then(() => getOllieBoilerplateConfig(projectPath))
+      .then((ollieConfig) => inquirer.prompt(ollieConfig.replacementQuestions))
+      .then((replacementAnswers) => seekAndReplace(answers.projectName, replacementAnswers, projectPath))
       .then(() => gitHandler.initializeGit())
       .then(() => gitHandler.initialCommit())
       .then(() => gitHandler.createRemoteRepository())
