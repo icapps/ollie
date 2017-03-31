80bfb691b6a9d512ff52acf07ad1c810d594a7be
hoetmaaiers <houdmeyers@gmail.com>
2017-03-31 10:09:30 +0200

    Add —dev mode check

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+import program from 'commander';
 import Ollie from './Ollie';
 import { projectTypes } from './constants';
 import surveys from './surveys';
 
-const ollie = new Ollie({ projectTypes, surveys });
-ollie.basic();
+program
+  .version('0.0.1')
+  .option('-d, --dev', 'Run in development mode')
+  .parse(process.argv);
+
+if (program.dev) {
+  console.log('Run in development mode');
+} else {
+  const ollie = new Ollie({ projectTypes, surveys });
+  ollie.start();
+}
